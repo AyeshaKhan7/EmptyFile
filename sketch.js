c1eafff696d70bc3d9fccaf7f0901b3f5c41716f
Ayesha Khan <31661317+AyeshaKhan7@users.noreply.github.com>
2017-09-13 11:42:34 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+//Ayesha Khan
+//9/13/17
+
+function setup () {
+  createCanvas(800,900);
+}
+
+
+function draw() {
+background(50, 100);
+fill(63, 145, 227);
+ellipse(mouseX,mouseY, 20, 20);
+console.log("check, check, ");
+}
